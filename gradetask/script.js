0f4231725ed33f38a3c797347bb47f04976d0f32
ZahidKhan4001 <zahidk4001@gmail.com>
2024-07-20 11:42:55 +0500

    add new assigbments

--- gradetask/script.js
+++ gradetask/script.js
@@ -1,14 +1,32 @@
-var marks = prompt("Please Enter your Marks");
+// var marks = prompt("Please Enter your Marks");
 
-if (marks >= 90) {
-    console.log("your grade is A+");
+// if (marks >= 90) {
+//     console.log("your grade is A+");
     
-} else if (marks <= 89 && marks >= 85) {
-    console.log("Your Grade is A");
+// } else if (marks <= 89 && marks >= 85) {
+//     console.log("Your Grade is A");
     
-} else if (marks <= 84 && marks >= 80) {
-    console.log("Your Grade is A−");
+// } else if (marks <= 84 && marks >= 80) {
+//     console.log("Your Grade is A−");
 
-} else {
-    console.log("You are Fail");
+// } else {
+//     console.log("You are Fail");
+// }
+
+// var fruits = ["Apple", "Mango", "Banana", "Orange", "Pinapple"];
+// for (var fruitsindex = 0; fruitsindex < fruits.length; fruitsindex++) {
+//     console.log(fruits);
+    
+// }
+
+// var fruits = [];
+// for (var fruitsindex = 0; fruitsindex < 5; fruitsindex++) {
+//     const fruitsname = prompt("Please Enter Fruits Name");
+//     console.log(fruits);
+    
+// }
+
+var num = [1, 3, 8, 9, 12, 13];
+for (var numindex = 0; numindex < num.length; num++) {
+    console.log(num);  
 }
---
+++ js-assignment-10/script.js
@@ -0,0 +1,20 @@
+// JAVASCRIPT PRACTICE PROBLEMS - 2
+
+// NOTE: HAR PROGRAM KLYE (JAHA ARRAY REQUIRED HA) ARRAY USER SE LE JIS ME 5 ELEMENTS HO
+
+// 6. FIND THE SMALLEST NUMBER IN AN ARRAY
+var numbers = []
+for (var numindex = 0; numindex < 5; numindex++) {
+    var userInput = +prompt("Please Enter Five Number!");
+    console.log({numindex});   
+}
+
+
+
+// 7. CONVERT CELCIUS TO FAHRENHEIT
+
+// 8. CONVERT FAHRENHEIT TO CELCIUS
+
+// 9. REVERSE AN ARRAY
+
+// 10. CALCULATE AVERAGE OF AN ARRAY
